2c48b365e76e5b8760109b2fe96444627cef379c
Matt Perille <matthew.perille@gmail.com>
2018-02-24 18:17:28 -0600

    Adding router

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -19,13 +19,8 @@ router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
 
-
-// when new recipe added, ensure has required fields. if not,
-// log error and return 400 status code with hepful message.
-// if okay, add new item, and return it with a status 201.
 router.post('/', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'ingredients'];
+  const requiredFields = ['title', 'content','author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -34,24 +29,18 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = BlogPosts.create(req.body.name, req.body.ingredients);
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
-// Delete BlogPosts (by id)!
 router.delete('/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  console.log(`Deleted blog posts item \`${req.params.ID}\``);
   res.status(204).end();
 });
 
-// when PUT request comes in with updated recipe, ensure has
-// required fields. also ensure that recipe id in url path, and
-// recipe id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `BlogPosts.updateItem` with updated recipe.
 router.put('/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'ingredients', 'id'];
+  const requiredFields = ['title', 'content','author','publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -67,12 +56,14 @@ router.put('/:id', jsonParser, (req, res) => {
     console.error(message);
     return res.status(400).send(message);
   }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
+  console.log(`Updating blog posts item \`${req.params.id}\``);
   const updatedItem = BlogPosts.update({
     id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+    });
   res.status(204).end();
 })
 
